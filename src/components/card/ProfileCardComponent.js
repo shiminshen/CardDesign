f709b58857f3dd4ad531e7341a45c4fe72d0a5dc
smshen <smshen17@gmail.com>
2016-03-07 17:38:02 +0800

    add react-motion

--- src/components/card/ProfileCardComponent.js
+++ src/components/card/ProfileCardComponent.js
@@ -4,19 +4,24 @@ import React from 'react';
 
 import AttributeList from './AttributeListComponent.js';
 import BasicInfo from './BasicInfoComponent.js';
+import { Motion, spring } from 'react-motion';
 
 require('styles/card/ProfileCard.styl');
 require('bootstrap/dist/css/bootstrap.min.css');
 
 class ProfileCardComponent extends React.Component {
+
   render() {
 
-
     return (
-      <div className="profilecard-component col-md-4 col-md-offset-4">
-        <BasicInfo/>
-        <AttributeList/>
-      </div>
+      <Motion defaultStyle={{opacity: 0}} style={{opacity: spring(10)}}>
+        { interpolatingStyle => 
+        <div style={interpolatingStyle} className="profilecard-component col-md-4 col-md-offset-4">
+          <BasicInfo/>
+          <AttributeList/>
+        </div>
+        }
+      </Motion>
     );
   }
 }
