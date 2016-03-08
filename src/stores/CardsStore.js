4ef9907bc62c82ce12388a556042cae53e21bcd1
smshen <smshen17@gmail.com>
2016-03-08 12:00:22 +0800

    add store

---
+++ src/stores/CardsStore.js
@@ -0,0 +1,49 @@
+import alt from 'components/Dispatcher';
+
+export class CardsStore {
+
+  constructor() {
+    this.cards = [
+      {
+        Apperance: {
+          eye: 3,
+          nose: 5,
+          mouse: 2
+        },
+        Personality: {
+          p1: 3,
+          p2: 2
+        },
+        Relationship: {
+          r1: 2,
+          r2: 1
+        },
+        Job: {
+          j1: 2,
+          j2: 3
+        }
+      }, {
+        Apperance: {
+          eye: 3,
+          nose: 5,
+          mouse: 2
+        },
+        Personality: {
+          p1: 3,
+          p2: 2
+        },
+        Relationship: {
+          r1: 2,
+          r2: 1
+        },
+        Job: {
+          j1: 2,
+          j2: 3
+        }
+      }
+    ];
+    // this.bindListeners({});
+  }
+}
+
+export default alt.createStore(CardsStore, 'CardsStore');
