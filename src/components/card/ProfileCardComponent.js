b713dfeef6255a84a67d036e36852dca1aebb2ef
smshen <smshen17@gmail.com>
2016-03-09 09:52:09 +0800

    connect store to component

--- src/components/card/ProfileCardComponent.js
+++ src/components/card/ProfileCardComponent.js
@@ -4,24 +4,25 @@ import React from 'react';
 
 import AttributeList from './AttributeListComponent.js';
 import BasicInfo from './BasicInfoComponent.js';
-import { Motion, spring } from 'react-motion';
+import CardsStore from '../../stores/CardsStore.js';
 
 require('styles/card/ProfileCard.styl');
 require('bootstrap/dist/css/bootstrap.min.css');
 
 class ProfileCardComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = CardsStore.getState();
+  }
 
   render() {
+    console.log(this.state);
 
     return (
-      <Motion defaultStyle={{opacity: 0}} style={{opacity: spring(10)}}>
-        { interpolatingStyle => 
-        <div style={interpolatingStyle} className="profilecard-component col-md-4 col-md-offset-4">
-          <BasicInfo/>
-          <AttributeList/>
-        </div>
-        }
-      </Motion>
+      <div className="profilecard-component col-md-4 col-md-offset-4">
+        <BasicInfo/>
+        <AttributeList/>
+      </div>
     );
   }
 }
